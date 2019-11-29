5c3d9596173c749acf7f6b3f8cadfa5a56aa1cdb
Francisco Ortiz <fortiz40.fo@gmail.com>
2019-11-29 11:25:51 -0800

    Making changes to CatController

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class CatController : MonoBehaviour
 {
+
+    private Rigidbody2D m_rigidbody;
+    private bool grounded = true;
+
+    [SerializeField] int move_speed = 5;
+    [SerializeField] int jump_force = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -15,17 +22,32 @@ public class CatController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.RightArrow)){
             Debug.Log("Right Key Pressed");
+
+            m_rigidbody.velocity = transform.right * move_speed;
         }
 
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             Debug.Log("Left Key Pressed");
+            m_rigidbody.velocity = -transform.right * move_speed;
         }
 
         if (Input.GetKey(KeyCode.Space))
         {
             Debug.Log("Space Pressed");
+            m_rigidbody.AddForce(Vector2.up * jump_force);
         }
 
     }
+
+    void OnJump()
+    {
+        grounded = false;
+    }
+
+    void OnLand()
+    {
+        grounded = true;
+    }
+
 }
