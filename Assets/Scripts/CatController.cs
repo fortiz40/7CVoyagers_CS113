0d4278ae795685d286ba3a922163d5f17361e3e8
Francisco Ortiz <fortiz40.fo@gmail.com>
2019-11-29 10:16:26 -0800

    Adding all Unity files

---
+++ Assets/Scripts/CatController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)){
+            Debug.Log("Right Key Pressed");
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Debug.Log("Left Key Pressed");
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Space Pressed");
+        }
+
+    }
+}
