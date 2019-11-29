bcea0d901ee36ead3b01bec6bf3169b76deb40c1
Francisco Ortiz <fortiz40.fo@gmail.com>
2019-11-29 14:50:25 -0800

    Added basic Player movement

--- Assets/Scripts/CatController.cs
+++ Assets/Scripts/CatController.cs
@@ -8,8 +8,9 @@ public class CatController : MonoBehaviour
     private Rigidbody2D m_rigidbody;
     private bool grounded = true;
 
-    [SerializeField] int move_speed = 5;
-    [SerializeField] int jump_force = 5;
+    [SerializeField] float move_speed = 0.5f;
+    [SerializeField] float jump_force = 5;
+    [SerializeField] public float gravity = 20.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,34 +21,51 @@ public class CatController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow)){
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
             Debug.Log("Right Key Pressed");
 
-            m_rigidbody.velocity = transform.right * move_speed;
+            transform.Translate(Vector3.right * move_speed * Time.deltaTime);
         }
 
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             Debug.Log("Left Key Pressed");
-            m_rigidbody.velocity = -transform.right * move_speed;
+            transform.Translate(Vector3.left * move_speed * Time.deltaTime);
         }
+
 
         if (Input.GetKey(KeyCode.Space))
         {
             Debug.Log("Space Pressed");
-            m_rigidbody.AddForce(Vector2.up * jump_force);
+            OnJump();
         }
 
     }
 
     void OnJump()
     {
-        grounded = false;
+        if (grounded == true)
+        {
+            grounded = false;
+            m_rigidbody.AddForce(Vector2.up * jump_force);
+        }
     }
 
     void OnLand()
     {
         grounded = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+
+        if (collision.gameObject.tag == "Ground")
+        {
+            OnLand();
+        }
+   
     }
 
 }
