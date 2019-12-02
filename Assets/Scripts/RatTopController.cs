8dcae15b193387cbe9018e441eaf8a0675e86953
Francisco Ortiz <fortiz40.fo@gmail.com>
2019-12-02 10:40:05 -0800

    Created Rat prefab along with its basic functionality Rat is destroyed when Cat lands on it. Rat damages Cat when it enters CatHitbox

---
+++ Assets/Scripts/RatTopController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatTopController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        string colliderTag = collision.collider.tag;
+        Debug.Log("RAT TOP COLLIDED WITH" + colliderTag);
+        if (colliderTag == "Cat")
+        {
+            Destroy(transform.parent.gameObject);
+        }
+    }
+}
